6490b2e728678552009340a56d90e3dd4a75263a
amalia <amaliarasyid1999@gmail.com>
2021-07-30 12:41:44 +0700

    Initial commit

---
+++ app/src/main/java/com/amaliasrasyid/learn_jetpack_compose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.amaliasrasyid.learn_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.amaliasrasyid.learn_jetpack_compose.ui.theme.LearnJetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnJetpackcomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LearnJetpackcomposeTheme {
+        Greeting("Android")
+    }
+}
